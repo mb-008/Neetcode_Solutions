c792143fe2628005730f7e0ac11e30e875a75198
mb-008 <mukulbhardwaj947@gmail.com>
2024-07-10 21:28:43 +0530

    Product except self with comments

--- product_except_Self.cpp
+++ product_except_Self.cpp
@@ -6,12 +6,14 @@ public:
         int n = nums.lenght();
         vector<int> left(n);
         left[0] = 1;
+        // element ke left side ke sare elements ka product
         for (int i = 1; i < n; ++i)
         {
             left[i] = left[i - 1] * nums[i - 1];
         }
         vector<int> right(n);
         right[n - 1] = 1;
+        //element ke right side ke sare elements ka product 
         for (int i = n - 2; i > -1; --i)
         {
             right[i] = right[i + 1] * nums[i + 1];
